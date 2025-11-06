f9399680977e5555794e1807186f2a4936c49586
Luis <77978673+OrangeXR@users.noreply.github.com>
2025-11-05 21:49:28 -0600

    Folder Rename

---
+++ 02/Assignment 2 - Basic/Assignment2 .cpp
@@ -0,0 +1,14 @@
+// Luis Morales
+// Assignment 2
+// 6-09-2020
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Luis Morales\n" << "120 *********. \n" << "San Antonio, TX *******\n" << "(210) ***-****\n" << "**********\n\n" ;
+	
+	system("PAUSE");
+	return 0;
+	}
+
