de8061b0fbf89fa0a4fe103954e8bbad371f090a
Luis <77978673+OrangeXR@users.noreply.github.com>
2025-11-05 21:51:01 -0600

    Folder Rename

---
+++ 07/Assignment7-ExtraCredit.cpp
@@ -0,0 +1,106 @@
+// Luis Morales
+// Assignment 7 - Student Line up
+// June 23, 2020
+
+/* 
+				ASSIGNMENT:	A7 – Student Line Up
+				"Student Line Up", page 295, Number 14
+				A teacher has asked all students to line up single file according 
+				to their first name. For example, in one class Amy will be in the 
+				front of the line and Yolanda will be at the end.
+
+				Write a program that prompts the user to enter the number of 
+				students in the class, then loops to read in that many names. Once 
+				all the names have been read in, it reports which student is in 
+				the front of the line and which student is at the end of the line. 
+				You may assume that no two students have the same name. This is not
+				 a sorting assignment.
+				
+				Input Validation: Do not let the user enter a number less than 1 of 
+				greater than 25 for the number of students.
+
+				EXTRA CREDIT: even though you do not have to sort this array, just 
+				tell who is first and who is last in line, if you sort it and then 
+				tell me who is first and who is last based on the sorted names, you 
+				will receive extra credit (hint, your text covers sorting, there are 
+				several ways to do this). (You will receive an extra 1 point if you can do this).
+*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	const int stdMin = 1;
+	const int stdMax = 25;
+	int stdTotal;
+	string stdName, first, last;
+
+	do
+		{
+				cout << "How many students are in your class? ";
+				cin >> stdTotal;
+				cin.ignore();
+
+				// Check if number between 1 and 25 is entered
+				if (stdTotal < stdMin || stdTotal > stdMax)
+					cout << "The number of students must be between 1 and 25.\n\n";
+		}
+	while ( stdTotal < stdMin || stdTotal > stdMax);
+
+	// get the first student name
+	cout << "\nEnter the name of a student: ";
+	getline(cin, stdName);
+	
+	// this will set the first and last
+	first = stdName;
+	last = stdName;
+	
+	// this loop will get the rest of the students' names and find first/last
+	for (int students = 2; students <= stdTotal; students++)
+	{
+	// next name
+	cout << "Enter the name the next student: ";
+	getline(cin,stdName);
+	// sort
+	if (stdName < first)
+		first =stdName;
+	if (stdName > last)
+		last = stdName;}
+	//display the first student in line
+	cout << endl << first << " is the first student in line.\n";
+	//display the last student in line
+	cout <<endl << last << " is the last student in line.\n";
+	
+	system("PAUSE");
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 08/Assignment8.cpp
@@ -0,0 +1,83 @@
+// Luis Morales
+// Assignment 8
+// June 26, 2020
+
+/*
+ Using Files-Numeric Processing
+=====================================================
+If you have downloaded this book's source code from the companion Web site, you
+will find a file named Random.txt in the Chapter 05 folder.  (The companion Web
+site is at www.pearsonhighered.com/gaddis.) This file contains a long list of random
+members.  Copy the file to your hard drive and then write a program that opens the file,
+reads all the numbers from the file, and calculates the following:
+
+A.) The number of numbers in the file
+B.) The sum of all the numbers in the file (a running total)
+C.)The average of all the numbers in the file
+
+The program should display the number of number found in the file, the sum of the 
+numbers, and the average of the numbers.
+=====================================================
+Use this txt file of numbers as your input file: Random.txt Preview the document
+Above is the file you will read in, Download it to your PC  ("Random.txt") and place in the 
+same folder as your C++ file for this assignment.  Write a program that reads all the number 
+in the "Random.txt" file and calculates the following:
+
+a. The number of numbers is the file (the final count)
+b. The sum of all the numbers in the file (total)
+c. The average of all the numbers in the file (total/count)
+
+Your program should then display the number of numbers found in the file, the sum of the numbers, and the average of the numbers.
+*/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+int main() 
+{
+    ifstream inputFile;
+
+    double sum, count, avg, num;
+
+    // Open and link input file
+    inputFile.open("Random.txt");
+
+	// Check
+    if (inputFile.fail())
+    {
+    	// Invalid
+        cout << "\n - Error opening file. - \n" << endl;
+    }
+    else 
+    {
+        // Valid
+        // Getting File info
+        while(inputFile >> num)
+        {
+        	// Add numbers up
+            sum += num;
+            // Count individual numbers
+            count++;
+        }
+        // Calculate average
+        avg = sum / count;
+
+
+	// Show total number of numbers in the file
+	cout << "A.) The number of numbers in the file is: "<< count << endl;
+	
+	// Show sum of all numbers in the file
+	cout << "B.) The sum of all the numbers in the file is: " << sum << endl;
+	
+	// Show average of all the numbers in the file
+	avg = sum / count;
+	cout << "C.) The average of all the numbers in the file is " << avg << endl;
+
+    }
+    // Close file after use
+	inputFile.close();
+ 	cout << "\n======\n\n" ; 
+	system("PAUSE");
+	return 0;
+}
